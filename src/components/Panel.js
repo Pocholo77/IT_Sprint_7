4ba6e93baeeccab408808094f86c535c39656610
Eduardo Wiater <kpooh7766@gmail.com>
2021-09-04 21:25:50 +0200

    Style: Add & modify Panel propeties

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,44 +1,48 @@
 import styled from "styled-components";
+import { PanelSelector } from "./PanelSelector";
 
 export const PanelStyled = styled.div`
+
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content:center;
+  justify-content: center;
   background: white;
   border: 2px solid black;
   border-radius: 10px;
   height: 9em;
+  width: 60%;
 
   & .PanelInputGroup {
     display: flex;
     flex-direction: row;
+    justify-content: center;
     align-items: center;
     
   }
+
   & div input {
     margin: 5px;
+    width: 30%;
+  }
+
+  & p{
+    margin-right: 5px;
   }
 `;
-export function Panel({ children , pages, languages, handleLanguages, handlePage}) {
+export function Panel({ children , pages, languages, handleLanguages, handlePage , handleClick}) {
   return (
     <PanelStyled>
       <div className="PanelInputGroup">
         <p>Numeros de Paginas:</p>
-        <input type="text" 
-        name="four" 
-        value={pages} 
-        onChange={handlePage} />
+        <PanelSelector name="four" value={pages} onChange={handlePage} handleClick={handleClick}/>
       </div>
+      
       <div className="PanelInputGroup">
         <p>Numeros de Idiomas:</p>
-        <input
-          type="text"
-          name="five"
-          value={languages}
-          onChange={handleLanguages}
-        />
+        <PanelSelector name="five" value={languages} onChange={handleLanguages} handleClick={handleClick}/> 
       </div>
+      
     </PanelStyled>
   );
 }
